9001a3afea54edf08b71d36462827ee3e74aa1c6
lk125454242 <125454242@qq.com>
2016-08-22 18:52:08 +0800

    增加 分页

--- routes/index.js
+++ routes/index.js
@@ -4,30 +4,38 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var List = mongoose.model('List');
 
-router.get('/', function(req, res, next) {
-  res.redirect('/register.html');
+router.get('/', function (req, res, next) {
+    res.redirect('/register.html');
 });
 
 var tt = 0;
-router.get('/time', function(req, res, next) {
-  res.set('Content-Type','text/event-stream');
-  res.write("data: {\"now\":"+Date.now()+"}\r\n\r\n");
-  res.end();
+router.get('/time', function (req, res, next) {
+    res.set('Content-Type', 'text/event-stream');
+    res.write("data: {\"now\":" + Date.now() + "}\r\n\r\n");
+    res.end();
 });
 router.search('/search', function (req, res) {
-  res.success({
-    code: 200,
-    message: '搜索'
-  });
-});
-router.get('/getList',function (req, res) {
-  List.find({},function (err, lists) {
     res.success({
-      code: 200,
-      data:lists,
-      message: '成功'
+        code: 200,
+        message: '搜索'
     });
-  })
+});
+router.get('/getList', function (req, res) {
+    var param = req.query, opts = {};
+    var timestamp = param.timestamp;
+    if (timestamp) {
+        opts = {timestamp: {$gt: timestamp}}
+    }
+    List.find(opts).limit(+param.size).then(function (lists) {
+        res.success({
+            code: 200,
+            data: lists,
+            message: '成功'
+        });
+    }).catch(function (err) {
+        res.error('网络链接错误');
+    })
+
 });
 module.exports = router;
 
